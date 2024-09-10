68483efad1c285e8cc5657e6e4211c841557f3df
Mike Harder <mharder@microsoft.com>
2024-09-10 20:31:05 +0000

    Move script to separate file with typings

---
+++ .github/workflows/scripts/add-label-1.js
@@ -0,0 +1,10 @@
+// @ts-check
+/** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
+export default async ({ github, context}) => {
+  await github.rest.issues.addLabels({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.payload.pull_request?.number,
+    labels: ['label1']
+  });
+}
