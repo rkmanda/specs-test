fff7d5a561d69f2308e39fd0dc2815635cea7939
Mike Harder <mharder@microsoft.com>
2024-09-17 19:55:40 +0000

    Add action arm-auto-signoff

--- .github/workflows/scripts/add-label.js
+++ .github/workflows/scripts/add-label.js
@@ -1,10 +1,10 @@
 // @ts-check
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
-module.exports = async ({ github, context}) => {
+module.exports = async ({ github, context, label}) => {
   await github.rest.issues.addLabels({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.payload.pull_request?.number,
-    labels: ['label1']
+    labels: [label],
   });
-}
+};
