d4dc586ef8929495e64da8e453e52cb9fdbd2835
Roopesh Manda <rmanda@microsoft.com>
2025-01-15 22:22:23 -0800

    minor change

--- .github/workflows/scripts/arm-new-incremental-typespec.js
+++ .github/workflows/scripts/arm-new-incremental-typespec.js
@@ -130,7 +130,7 @@ async function isSwaggerFileGeneratedFromTypeSpec(file, fromHead = false) {
         // Create a temporary directory
         const tempDir = await mkdtemp(path.join(os.tmpdir(), 'git-'));
         // Read the file from the HEAD commit
-        swagger = await util.execRoot(`git show HEAD:${file}`);
+        swagger = await util.execRoot(`git show HEAD^:${file}`);
       } else {
         // Read the file from the current branch
         filePath = path.join(process.env.GITHUB_WORKSPACE || "", file);
